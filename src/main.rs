5adf89151c1d1ddcfd710ac1c3dbe593e29f7ef4
crjeder <crjeder@gmail.com>
2021-03-22 14:53:51 +0100

    added test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi, Segment};
+use std::{thread, time};
 
 use hx711_spi::Hx711;
 
 fn main()
 {
         let mut test = Hx711::new(rppal::spi::Bus::Spi0).unwrap();
-        let v = test.readout().unwrap();
-        println!("value = {}", v);
+	loop
+	{
+        	let v = test.readout().unwrap();
+        	println!("value = {}", v);
+		thread::sleep(time::Duration::from_millis(100));
+	}
 }
