d2ac6fee7e8300ab2aa585218e08afa5cdc4267c
Christoph <crjeder@gmail.com>
2024-05-15 08:15:07 +0200

    working version

--- examples/raspberry.rs
+++ examples/raspberry.rs
@@ -1,17 +1,16 @@
 // embedded_hal implementation
 use rppal::spi::{Bus, Error, Mode, SlaveSelect, Spi};
 
-use hx711_spi::Hx711;
-use nb::block;
+use hx711_spi::{Hx711, Hx711Error};
 
 // minimal example
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Hx711Error<Error>> {
     let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode1)?;
     let mut hx711 = Hx711::new(spi);
 
     hx711.reset()?;
-    let v = block!(hx711.read())?;
-    println!("value = {}", v);
+    let v = hx711.read();
+    println!("value = {:?}", v);
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,7 @@
 use bitmatch::bitmatch;
 use core::unimplemented;
 use embedded_hal as hal;
-use hal::blocking::spi;
-use nb::{self, block};
+use hal::spi::SpiBus;
 
 // Bit pattern definitions for the communication with the hx711. All have to be bitwise negated
 // for the ```invert-sdo``` feature
@@ -71,15 +70,27 @@ pub struct Hx711<SPI> {
     mode: Mode,
 }
 
-impl<SPI, E> Hx711<SPI>
+#[derive(Copy, Clone, Debug)]
+pub enum Hx711Error<SPI> {
+    Spi(SPI),
+    DataNotReady,
+}
+
+impl<SPIERROR> From<SPIERROR> for Hx711Error<SPIERROR> {
+    fn from(value: SPIERROR) -> Self {
+        Hx711Error::Spi(value)
+    }  
+}
+
+impl<SPI> Hx711<SPI>
 where
-    SPI: spi::Transfer<u8, Error = E>, // + spi::Write<u8, Error = E>,
+    SPI: SpiBus,
 {
     /// opens a connection to a HX711 on a specified SPI.
     ///
-    /// The datasheet specifies PD_SCK high time and PD_SCK low time to be in the 0.2 to 50 us range,
+    /// The data sheet specifies PD_SCK high time and PD_SCK low time to be in the 0.2 to 50 us range,
     /// therefore bus speed has to be between 5 MHz and 20 kHz. 1 MHz seems to be a good choice.
-    /// D is an embedded_hal implementation of DelayMs.
+    /// D is an `embedded_hal` implementation of `DelayMs`.
     pub fn new(spi: SPI) -> Self {
         Hx711 {
             spi,
@@ -89,38 +100,33 @@ where
 
     /// reads a value from the HX711 and returns it
     /// # Errors
-    /// Returns SPI errors and nb::Error::WouldBlock if data isn't ready to be read from hx711
-    pub fn read(&mut self) -> nb::Result<i32, E> {
+    /// Returns `SPI` errors and `nb`::Error::`WouldBlock` if data isn't ready to be read from hx711
+    pub fn read(&mut self) -> Result<i32, Hx711Error<SPI::Error>> {
         // check if data is ready
         // When output data is not ready for retrieval, digital output pin DOUT is high.
         // Serial clock input PD_SCK should be low. When DOUT goes
         // to low, it indicates data is ready for retrieval.
         let mut txrx: [u8; 1] = [SIGNAL_LOW];
 
-        self.spi.transfer(&mut txrx)?;
+        self.spi.transfer_in_place(&mut txrx)?;
 
         if txrx[0] & 0b01 == 0b01 {
             // as long as the lowest bit is high there is no data waiting
-            return Err(nb::Error::WouldBlock);
+            return Err(Hx711Error::DataNotReady);
         }
 
         let mut buffer: [u8; 7] = [CLOCK, CLOCK, CLOCK, CLOCK, CLOCK, CLOCK, self.mode as u8];
 
-        self.spi.transfer(&mut buffer)?;
+        self.spi.transfer_in_place(&mut buffer)?;
 
         Ok(decode_output(&buffer)) // value should be in range 0x800000 - 0x7fffff according to datasheet
     }
 
-    #[inline]
-    /// This is for compatibility only. Use [read]() instead.
-    pub fn retrieve(&mut self) -> nb::Result<i32, E> {
-        self.read()
-    }
     /// Reset the chip to it's default state. Mode is set to convert channel A with a gain factor of 128.
     /// # Errors
-    /// Returns SPI errors
+    /// Returns `SPI` errors
     #[inline]
-    pub fn reset(&mut self) -> Result<(), E> {
+    pub fn reset(&mut self) -> Result<(), Hx711Error<SPI::Error>>{
         // when PD_SCK pin changes from low to high and stays at high for longer than 60µs,
         // HX711 enters power down mode.
         // When PD_SCK returns to low, chip will reset and enter normal operation mode.
@@ -131,7 +137,7 @@ where
 
         let mut buffer: [u8; 301] = RESET_SIGNAL;
 
-        self.spi.transfer(&mut buffer)?;
+        self.spi.transfer_in_place(&mut buffer)?;
         self.mode = Mode::ChAGain128; // this is the default mode after reset
 
         Ok(())
@@ -139,11 +145,11 @@ where
 
     /// Set the mode to the value specified.
     /// # Errors
-    /// Returns SPI errors
+    /// Returns `SPI` errors
     #[inline]
-    pub fn set_mode(&mut self, m: Mode) -> Result<Mode, E> {
+    pub fn set_mode(&mut self, m: Mode) -> Result<Mode, Hx711Error<SPI::Error>> {
         self.mode = m;
-        block!(self.read())?; // read writes Mode for the next read()
+        self.read()?; // read writes Mode for the next read()
         Ok(m)
     }
 
@@ -153,18 +159,12 @@ where
         self.mode
     }
 
-    #[inline]
-    /// This is for compatibility only. Use [mode]() instead.
-    pub fn get_mode(&mut self) -> Mode {
-        self.mode
-    }
-
     /// To power down the chip the PD_SCK line has to be held in a 'high' state. To do this we
-    /// would need to write a constant stream of binary '1' to the SPI bus which would totally defy
+    /// would need to write a constant stream of binary '1' to the `SPI` bus which would totally defy
     /// the purpose. Therefore it's not implemented.
     // If the SDO pin would be idle high (and at least some MCU's seem to do that in mode 1) then the chip would automatically
     // power down if not used. Cool!
-    pub fn disable(&mut self) -> Result<(), E> {
+    pub fn disable(&mut self) -> Result<(), Hx711Error<SPI::Error>> {
         // when PD_SCK pin changes from low to high and stays at high for longer than 60µs, HX711 enters power down mode
         // When PD_SCK returns to low, chip will reset and enter normal operation mode.
         // this can't be implemented with SPI because we would have to write a constant stream
@@ -173,7 +173,7 @@ where
     }
 
     /// Power up / down is not implemented (see disable)
-    pub fn enable(&mut self) -> Result<(), E> {
+    pub fn enable(&mut self) -> Result<(), Hx711Error<SPI::Error>> {
         // when PD_SCK pin changes from low to high and stays at high for longer than 60µs, HX711 enters power down mode
         // When PD_SCK returns to low, chip will reset and enter normal operation mode.
         // this can't be implemented with SPI because we would have to write a constant stream
@@ -214,10 +214,10 @@ fn decode_output(buffer: &[u8; 7]) -> i32 {
 #[cfg(test)]
 //#[macro_use]
 //extern crate std;
-mod tests {  
+mod tests {
     use super::*;
     use test_case::test_case;
-  
+
     #[test_case(&[0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55] => 0; "alternating convert to zeros")]
     #[test_case(&[0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA] => -1; "alternating convert to ones")]
     #[test_case(&[0xFF, 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF] => -1; "all ones")]
@@ -225,5 +225,5 @@ mod tests {
                   0b00100111, 0b00100111, 0b00100111] => 0b0000_0000_0101_0101_0101_0101_0101_0101i32; "test pattern")]
     fn test_decode(buffer: &[u8; 7]) -> i32 {
         decode_output(&buffer)
-    }   
+    }
 }
