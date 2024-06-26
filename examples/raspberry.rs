d6224b7dceb6289f011ebd2556ff7f3571c970ee
Christoph <crjeder@gmail.com>
2024-06-26 19:29:48 +0200

    sync'ed (not really) example

--- examples/raspberry.rs
+++ examples/raspberry.rs
@@ -1,11 +1,13 @@
 // embedded_hal implementation
-use rppal::spi::{Bus, Error, Mode, SlaveSelect, Spi};
+use rppal::spi::{Bus, Error, Mode, SimpleHalSpiDevice, SlaveSelect, Spi};
 
 use hx711_spi::{Hx711, Hx711Error, Mode as HxMode};
 
-// minimal example
-fn main() -> Result<(), Hx711Error<Error>> {
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode1)?;
+// minimal sync example
+#[cfg(feature = "is_sync")]
+fn main() -> Result<(), Hx711Error<Error>> {    
+    let spi_device = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode1)?;
+    let spi_bus = SimpleHalSpiDevice::new(spi_device)?;
     let mut hx711 = Hx711::new(spi);
 
     hx711.reset()?;
