df69504612ede37d405a4424ebf7973f5dc1823c
Christoph <crjeder@gmail.com>
2024-06-27 09:43:11 +0200

    add cache to runner

--- examples/raspberry.rs
+++ examples/raspberry.rs
@@ -4,8 +4,9 @@ use rppal::spi::{Bus, Error, Mode, SimpleHalSpiDevice, SlaveSelect, Spi};
 use hx711_spi::{Hx711, Hx711Error, Mode as HxMode};
 
 // minimal sync example
-#[cfg(feature = "is_sync")]
-fn main() -> Result<(), Hx711Error<Error>> {    
+fn main() -> Result<(), Hx711Error<Error>> {   
+    #[cfg(feature = "is_sync")] 
+    {
     let spi_device = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode1)?;
     let spi_bus = SimpleHalSpiDevice::new(spi_device)?;
     let mut hx711 = Hx711::new(spi);
@@ -14,6 +15,7 @@ fn main() -> Result<(), Hx711Error<Error>> {
     hx711.set_mode(HxMode::ChAGain128)?;
     let v = hx711.read()?;
     println!("value = {:?}", v);
-
+    }
     Ok(())
+    
 }
